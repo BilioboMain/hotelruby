7b7a50a6e28cafc2f4699777e575b143193b95df
Gleb <glebkrotovrus@gmail.com>
2021-09-06 20:22:35 +0300

    added prices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_204347) do
+ActiveRecord::Schema.define(version: 2021_09_06_171341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_09_05_204347) do
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
   end
 
 end
