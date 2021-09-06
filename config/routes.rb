cb2a6daa96c0c6ed039797a45384d71978e9884d
Gleb <glebkrotovrus@gmail.com>
2021-09-06 10:55:56 +0300

    added main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'bookings_admin', to: 'bookings_admin#index'
   resources :rooms_admins
   get 'reviews_admin/index'
+  get '/', to: "mainpage#index"
   devise_for :admins
+  root 'reviews_admin#index'
   get 'admins_panel', to:'admins_panel#index'
   get 'reviews_admin', to:'reviews_admin#index'
   post '/reviews_admin/update', to:'reviews_admin#update'
@@ -18,7 +20,7 @@ Rails.application.routes.draw do
   get 'rooms/:id', to:"rooms#show"
   get 'reviews/new', to: 'reviews#new'
   post 'reviews/create', to: 'reviews#create'
-  root to: "admins_panel#index"
+  #root to: "admins_panel#index"
   #get 'rooms/:id', to:"rooms#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
