14de88d685c1744a42336e7d88dba2889be9955a
Gleb <glebkrotovrus@gmail.com>
2021-09-07 23:21:43 +0300

    background job

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'generator/', to: 'generator#index'
   get 'bookings_admin', to: 'bookings_admin#index'
   resources :rooms_admins
   get 'reviews_admin/index'
